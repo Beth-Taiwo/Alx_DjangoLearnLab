91d2f13bc3ea0839c54c870d2e697b3309d187bb
Beth Taiwo <popoolataiwo22@gmail.com>
2024-11-09 19:13:45 +0000

    Query all books by a specific author

--- django-models/relationship_app/query_samples.py
+++ django-models/relationship_app/query_samples.py
@@ -1,12 +1,17 @@
-from relationship_app.models import Book,Library
+from relationship_app.models import Author,Book,Library
+
+author_name = Book.objects.create(name="George Orwell")
+
+author = Author.objects.get(name=author_name)
 
 # Query all books by a specific author.
-books_by_author = Book.objects.filter(author__name="George Orwell")
+books_by_author = Book.objects.filter(author__name=author)
 
 for book in books_by_author:
     print(book.title)
     
 library_name = Library.objects.create(name="Custodian")
+library_name.save()
     
 # List all books in a library.
 library = Library.objects.get(name=library_name)
