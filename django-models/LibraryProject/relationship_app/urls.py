b37fc261dc001e2837e018a5c852e56ac7bc4851
Beth Taiwo <popoolataiwo22@gmail.com>
2024-11-09 23:24:59 +0000

    renamed function

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("books/", views.book_list, name="Book list"),
+    path("books/", views.list_books, name="Book list"),
     path("librarys/", views.LibraryDetailView.as_view(), name="Library Detail")
 ]
