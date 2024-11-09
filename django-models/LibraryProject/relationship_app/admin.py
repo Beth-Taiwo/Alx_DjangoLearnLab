463d445dd0647cc148bd8aee82102c61c80c2fac
Beth Taiwo <popoolataiwo22@gmail.com>
2024-11-09 21:32:34 +0000

    Django Views and URL Configuration

--- django-models/LibraryProject/relationship_app/admin.py
+++ django-models/LibraryProject/relationship_app/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from relationship_app.models import Author,Book,Library,Librarian
+
+admin.site.register(Author)
+admin.site.register(Book)
+admin.site.register(Library)
+admin.site.register(Librarian)
+
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from.models import Library, Book
 
 # Create your views here.
+def book_list(request):
+      """Retrieves all books and renders a template displaying the list."""
+      books = Book.objects.all()  # Fetch all book instances from the database
+      context = {'books': books}  # Pass the books to the context dictionary for rendering in the template
+      return render(request, 'relationship_app/book_list.html', context)
+
+
+# Create a class-based view in relationship_app/views.py that displays details for a specific library, listing all books available in that library.
+
+class LibraryDetailView(DetailView):
+      """Displays details for a specific library and lists all books available in that library."""
+      model = Library
+      template_name = 'relationship_app/library_detail.html'  # Template to use for rendering the view
+
+      def get_context_data(self, **kwargs):
+            context = super().get_context_data(**kwargs)  # Retrieve the context data from the superclass
+            library = context['library']  # Get the library instance from the context
+            context['books'] = library.books.all()  # Fetch all books associated with the library
+            return context
