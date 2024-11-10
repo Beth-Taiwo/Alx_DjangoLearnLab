85f32654d48d3a946447ba9bdbd3282adc1dc988
Beth Taiwo <popoolataiwo22@gmail.com>
2024-11-10 20:46:47 +0000

    provided logout file path

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path("books/", list_books, name="Book list"),
     path("librarys/", LibraryDetailView.as_view(), name="Library Detail"),
     path('login/', LoginView.as_view(template_name='registration/login.html'), name='login'),
-    path('logout/', LogoutView.as_view(), name='logout'),
+    path('logout/', LogoutView.as_view(template_name='registration/logout.html'), name='logout'),
 ]
