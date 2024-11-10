7a62c8173c9b83b27c38efdb6193ed76c6a075f4
Beth Taiwo <popoolataiwo22@gmail.com>
2024-11-10 22:15:19 +0000

    Setup Role-Based Access Control

--- django-models/LibraryProject/relationship_app/admin.py
+++ django-models/LibraryProject/relationship_app/admin.py
@@ -2,10 +2,10 @@ from django.contrib import admin
 
 # Register your models here.
 
-from relationship_app.models import Author,Book,Library,Librarian
+from relationship_app.models import Author,Book,Library,Librarian,UserProfile
 
 admin.site.register(Author)
 admin.site.register(Book)
 admin.site.register(Library)
 admin.site.register(Librarian)
-
+admin.site.register(UserProfile)
