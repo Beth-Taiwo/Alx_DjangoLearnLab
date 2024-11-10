168ac48708803cfbdc14bff883ec25a5c1b7862b
Beth Taiwo <popoolataiwo22@gmail.com>
2024-11-10 21:13:06 +0000

    moved htmls files to relationship_app

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -7,7 +7,7 @@ from . import views
 urlpatterns = [
     path("books/", list_books, name="Book list"),
     path("librarys/", LibraryDetailView.as_view(), name="Library Detail"),
-    path('login/', LoginView.as_view(template_name='registration/login.html'), name='login'),
-    path('logout/', LogoutView.as_view(template_name='registration/logout.html'), name='logout'),
+    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
     path('register/', views.register, name="Register"),
 ]
