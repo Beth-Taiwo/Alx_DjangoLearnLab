6a96d13864d7488064030a0c2859e98ed396562e
Beth Taiwo <popoolataiwo22@gmail.com>
2024-11-10 22:14:45 +0000

    Configured URL Patterns

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import list_books,LibraryDetailView
-from django.contrib.auth.views import LoginView, LogoutView
+from .views import list_books,LibraryDetailView,admin_view,librarian_view,member_view
 from . import views
 
 
@@ -10,4 +10,7 @@ urlpatterns = [
     path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
     path('register/', views.register, name="Register"),
+    path('admin_view/', admin_view, name="Admin view"),
+    path('librarian_view/', librarian_view, name="Librarian view"),
+    path('member_view/', member_view, name="Member view"),
 ]
