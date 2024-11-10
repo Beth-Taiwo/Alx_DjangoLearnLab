1c6bb9ea341ac8a29f01ae7c2901e070e1c8281c
Beth Taiwo <popoolataiwo22@gmail.com>
2024-11-10 20:54:42 +0000

    registered Registration path

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 from .views import list_books,LibraryDetailView
 from django.contrib.auth.views import LoginView, LogoutView
+from . import views
 
-views.register
 
 urlpatterns = [
     path("books/", list_books, name="Book list"),
     path("librarys/", LibraryDetailView.as_view(), name="Library Detail"),
     path('login/', LoginView.as_view(template_name='registration/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='registration/logout.html'), name='logout'),
+    path('register/', views.register, name="Register"),
 ]
