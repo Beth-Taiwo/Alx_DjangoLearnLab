300e028239b20ef4d1cd3d78243c009ebb5985e9
Beth Taiwo <popoolataiwo22@gmail.com>
2024-11-17 22:59:57 +0000

    removed comment

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -2,15 +2,6 @@ from django.shortcuts import render
 
 # Create your views here.
 
-'''
-Enforce Permissions in Views
-Modify your views to check for these permissions before allowing users to perform certain actions. Use decorators such as permission_required to enforce these permissions in your views.
-
-Views to Modify or Create:
-Ensure views that create, edit, or delete model instances check for the correct permissions.
-Example: Use @permission_required('app_name.can_edit', raise_exception=True) to protect an edit view.
-'''
-
 def list_books(request):
     """Retrieves all books and renders a template displaying the list."""
     books = Book.objects.all()
