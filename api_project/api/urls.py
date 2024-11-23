9c31b7598307f85cbeb2fa819122f1c9aed9a442
Beth Taiwo <popoolataiwo22@gmail.com>
2024-11-23 20:58:26 +0000

    Implemented Authentication and Permissions in Django REST Framework

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path, include
-from .views import BookList, BookViewSet
+from .views import BookList, BookViewSet,ProtectedView
 from rest_framework.routers import DefaultRouter
+from rest_framework.authtoken.views import obtain_auth_token
 
 router = DefaultRouter()
 router.register(r'books_all', BookViewSet, basename='books_all')
 
 urlpatterns = [
     path('books/', BookList.as_view(), name="book-list"),
-    path('', include(router.urls))
+    path('', include(router.urls)),
+    path('token/', obtain_auth_token, name='api_token_auth'),
+    path('protected/', ProtectedView.as_view(), name='protected_view'),
 ]
--- api_project/api/views.py
+++ api_project/api/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from .serializers import BookSerializer 
 from rest_framework import generics, viewsets
 from .models import Book
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
 class BookList(generics.ListAPIView):
@@ -11,3 +14,11 @@ class BookList(generics.ListAPIView):
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    
+
+class ProtectedView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        content = {'message': 'This is a protected view.'}
+        return Response(content)
