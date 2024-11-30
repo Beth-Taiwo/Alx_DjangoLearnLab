cc0766f3a2f45401a05dfc9b88056424502afa92
Beth Taiwo <popoolataiwo22@gmail.com>
2024-11-30 23:34:46 +0000

    provided path for generic views

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from .views import BookList, BookViewSet,AuthorView
+from .views import BookList, BookViewSet,AuthorView, BookCreateView, BookUpdateView, BookDeleteView, BookDetailView
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
@@ -9,4 +9,8 @@ urlpatterns = [
     path('books/', BookList.as_view(), name="book-list"),
     path('', include(router.urls)),
     path('authors/', AuthorView.as_view(), name='author_view'),
+    path('books/add/', BookCreateView.as_view(), name='book_add'),
+    path('books/<int:pk>/update/', BookUpdateView.as_view(), name='book_update'),
+    path('books/<int:pk>/delete/', BookDeleteView.as_view(), name='book_delete'),
+    path('books/<int:pk>/', BookDetailView.as_view(), name='book_detail'),
 ]
