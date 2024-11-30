61d0b23b14f07a2b319047ee0404a56a57a6336b
Beth Taiwo <popoolataiwo22@gmail.com>
2024-11-30 23:36:10 +0000

    updated paths

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -9,8 +9,8 @@ urlpatterns = [
     path('books/', BookList.as_view(), name="book-list"),
     path('', include(router.urls)),
     path('authors/', AuthorView.as_view(), name='author_view'),
-    path('books/add/', BookCreateView.as_view(), name='book_add'),
-    path('books/<int:pk>/update/', BookUpdateView.as_view(), name='book_update'),
-    path('books/<int:pk>/delete/', BookDeleteView.as_view(), name='book_delete'),
+    path('books/create', BookCreateView.as_view(), name='book_add'),
+    path('books/update', BookUpdateView.as_view(), name='book_update'),
+    path('books/delete', BookDeleteView.as_view(), name='book_delete'),
     path('books/<int:pk>/', BookDetailView.as_view(), name='book_detail'),
 ]
