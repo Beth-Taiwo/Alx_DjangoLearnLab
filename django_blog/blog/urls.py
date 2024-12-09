97c92694d7d9cb85647e0b4f1c8d5749fc0094f8
Beth Taiwo <popoolataiwo22@gmail.com>
2024-12-09 00:04:10 +0000

    appended additional path

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -17,4 +17,5 @@ urlpatterns = [
     path('post/<int:post_id>/comments/new/', views.CommentCreateView.as_view(), name='create_comment'),
     path('comment/<int:pk>/update/', views.CommentUpdateView.as_view(),name='comment-update'),
     path('comment/<int:pk>/delete/', views.CommentDeleteView.as_view(),name='comment-delete'),
+    path('post/<int:pk>/comments/new/',views.CommentCreateView.as_view(), name='create_comment'),
 ]
