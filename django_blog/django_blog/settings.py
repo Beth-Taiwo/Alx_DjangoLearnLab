bd253ede43911831717d8fd97d5938e0415a55a1
Beth Taiwo <popoolataiwo22@gmail.com>
2024-12-13 17:06:07 +0000

    defined LOGIN_URL

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'blog/static'),)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
@@ -132,3 +132,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 LOGIN_REDIRECT_URL = '/blog/home/'  # Redirect URL after login
+LOGIN_URL = '/blog/login' # Redirect URL to login
