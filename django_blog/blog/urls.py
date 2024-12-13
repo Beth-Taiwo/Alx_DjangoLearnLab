bdf52a264af610fd6d1aad40aa46dd0f97839e6e
Beth Taiwo <popoolataiwo22@gmail.com>
2024-12-13 21:57:33 +0000

    updated routes

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -5,17 +5,20 @@ from . import views
 
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name="blog/login.html"), name='login'),
-    path('logout/',views.logout_view, name='logout'),
+    path('logout/',auth_views.LogoutView.as_view(template_name="blog/logout.html"), name='logout'),
     path('register/', views.RegisterView.as_view(), name='register'),
     path('home/', views.home_view, name='home'),
     path('profile/', views.ProfileView.as_view(), name='profile'),
-    path('posts/', views.PostView.as_view(), name='posts'),
+    path('posts/', views.PostListView.as_view(), name='posts'),
     path('post/new/', views.PostCreateView.as_view(), name='post_create'),
     path('post/<int:pk>/', views.PostDetailView.as_view(), name='post_detail'),
     path('post/<int:pk>/update/', views.update_post, name='post_update'),
     path('post/<int:pk>/delete/', views.delete_post, name='post_delete'),
-    path('post/<int:post_id>/comments/new/', views.CommentCreateView.as_view(), name='create_comment'),
-    path('comment/<int:pk>/update/', views.CommentUpdateView.as_view(),name='comment-update'),
-    path('comment/<int:pk>/delete/', views.CommentDeleteView.as_view(),name='comment-delete'),
-    path('post/<int:pk>/comments/new/',views.CommentCreateView.as_view(), name='create_comment'),
+    path('post/<int:post_id>/comments/new/', views.CommentCreateView.as_view(), name='comment_create'),
+    path('comment/<int:pk>/update/', views.CommentUpdateView.as_view(),name='comment_update'),
+    path('comment/<int:pk>/delete/', views.CommentDeleteView.as_view(),name='comment_delete'),
+    path('tags/<tag_name>/', views.tag_view, name='post-tag'),
+    # path('tags/<slug:tag_slug>/', views.PostByTagListView.as_view(), name='post-tag_list'),
+    path('search/', views.SearchView.as_view(), name='post-search'),
+
 ]
