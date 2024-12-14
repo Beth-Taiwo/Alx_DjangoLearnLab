18c8e57cbf976955c2db9b1b2ea834fc0249f4e4
Beth Taiwo <popoolataiwo22@gmail.com>
2024-12-14 16:27:03 +0000

    setup signal to create a token for a user on account creation

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -3,6 +3,10 @@ from rest_framework import serializers
 from rest_framework.authtoken.models import Token
 
 class CustomUserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(
+          write_only=True,
+          style={'input_type': 'password', 'placeholder': 'Password'},
+    )
     class Meta:
         model = get_user_model()
         fields = ('id', 'username', 'bio', 'profile_picture', 'followers', 'password')
@@ -11,6 +15,14 @@ class CustomUserSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         user = get_user_model().objects.create_user(**validated_data)
         return user
+    
+    def create(self, validated_data):
+        user = get_user_model().objects.create_user(validated_data)
+        if 'password' in validated_data:
+            user.set_password(validated_data['password'])
+            user.save()
+            token = Token.objects.create(user=user)
+        return user, token
 
 class TokenSerializer(serializers.ModelSerializer):
     class Meta:
