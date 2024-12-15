15899740c8eb376f7d09108d6dee335b631a7e01
Beth Taiwo <popoolataiwo22@gmail.com>
2024-12-15 23:29:45 +0000

    configured mysql database

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -80,9 +80,12 @@ WSGI_APPLICATION = 'social_media_api.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+     'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'social_media_api',
+        'USER': 'root',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
