f64f3bf77c40b49f14040386b0ea7ed13e0c7811
Beth Taiwo <popoolataiwo22@gmail.com>
2024-12-15 00:51:04 +0000

    updated url to include like & unlike

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -10,4 +10,6 @@ post_router.register(r'comments', views.CommentViewSet, basename='comment')
 urlpatterns = [
     path('', include(post_router.urls)),
     path('feed/', views.FeedView.as_view(), name='feed'),
+    path('<int:pk>/like/', views.PostViewSet.like, name='like-post'),
+    path('<int:pk>/unlike/', views.PostViewSet.unlike, name='unlike-post'),
 ]
